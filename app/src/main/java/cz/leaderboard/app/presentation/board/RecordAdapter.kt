069f102ffe8e00aeb3e8e58e0fd858fc9f2e5574
Tomas Valenta <semanticer@gmail.com>
2017-06-18 07:36:38 +0200

    Avatars!

--- app/src/main/java/cz/leaderboard/app/presentation/board/RecordAdapter.kt
+++ app/src/main/java/cz/leaderboard/app/presentation/board/RecordAdapter.kt
@@ -6,36 +6,99 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import cz.leaderboard.app.R
+import cz.leaderboard.app.R.id.order
 import cz.leaderboard.app.domain.board.LeaderboardRecord
 import cz.leaderboard.app.presentation.common.ViewBinder
 import cz.leaderboard.app.presentation.common.bindView
+import cz.leaderboard.app.presentation.common.setRandomDrawable
+import de.hdodenhof.circleimageview.CircleImageView
 
 /**
  * Created by semanticer on 17.06.2017.
  */
-class RecordAdapter : RecyclerView.Adapter<RecordAdapter.RecordViewHolder>() {
+class RecordAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     val recordList = mutableListOf<LeaderboardRecord>()
+    val topTriad = mutableListOf<LeaderboardRecord>()
 
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecordViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_record, parent, false)
-        return RecordViewHolder(view)
+    override fun getItemViewType(position: Int): Int {
+        return if (position == 0) 0 else 1
     }
 
-    override fun onBindViewHolder(holder: RecordViewHolder, position: Int) {
-        val post = recordList[position]
-        holder.bind(position+1, post)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        if (viewType == 0) {
+            return TopViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_top_record, parent, false))
+        } else {
+            return RecordViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_record, parent, false))
+        }
     }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+
+        if (holder.itemViewType == 0) {
+            (holder as TopViewHolder).bind(topTriad)
+        } else {
+            val item = recordList[position - 1]
+            val order = position + 3
+            (holder as RecordViewHolder).bind(order, item)
+        }
+    }
+
 
     override fun getItemCount(): Int {
-        return recordList.size
+        return if (topTriad.isNotEmpty()) 1 + recordList.size else 0
     }
 
-    fun updateData(posts: List<LeaderboardRecord>) {
+    fun updateData(record: List<LeaderboardRecord>) {
+
+        topTriad.clear()
         recordList.clear()
-        recordList.addAll(posts)
+
+        for (r in record) {
+            if (topTriad.size < 3) {
+                topTriad.add(r)
+            } else {
+                recordList.add(r)
+            }
+        }
         notifyDataSetChanged()
+    }
+
+    class TopViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        internal val firstAvatar: CircleImageView by bindView(R.id.first_avatar)
+        internal val firstName: TextView by bindView(R.id.first_name)
+        internal val firstScore: TextView by bindView(R.id.first_score)
+
+        internal val secondAvatar: CircleImageView by bindView(R.id.second_avatar)
+        internal val secondName: TextView by bindView(R.id.second_name)
+        internal val secondScore: TextView by bindView(R.id.second_score)
+
+        internal val thirdAvatar: CircleImageView by bindView(R.id.third_avatar)
+        internal val thirdName: TextView by bindView(R.id.third_name)
+        internal val thirdScore: TextView by bindView(R.id.third_score)
+
+        init {
+            ViewBinder.setup(this, itemView)
+        }
+
+        fun bind(topThree: List<LeaderboardRecord>) {
+            if (topThree.isNotEmpty()) {
+                firstAvatar.setRandomDrawable(topThree[0].user)
+                firstName.text = topThree[0].user.name
+                firstScore.text = topThree[0].user.score.toString()
+            }
+            if (topThree.size > 1) {
+                secondAvatar.setRandomDrawable(topThree[1].user)
+                secondName.text = topThree[1].user.name
+                secondScore.text = topThree[1].user.score.toString()
+            }
+            if (topThree.size > 2) {
+                thirdAvatar.setRandomDrawable(topThree[2].user)
+                thirdName.text = topThree[2].user.name
+                thirdScore.text = topThree[2].user.score.toString()
+            }
+        }
     }
 
     class RecordViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -43,15 +106,19 @@ class RecordAdapter : RecyclerView.Adapter<RecordAdapter.RecordViewHolder>() {
         internal val order: TextView by bindView(R.id.order)
         internal val username: TextView by bindView(R.id.username)
         internal val score: TextView by bindView(R.id.score)
+        internal val avatar: CircleImageView by bindView(R.id.avatar)
 
         init {
             ViewBinder.setup(this, itemView)
         }
 
         fun bind(pos: Int, record: LeaderboardRecord) {
-            order.text = pos.toString()
+            order.text = "#${pos}"
             username.text = record.user.name
             score.text = record.user.score.toString() + " " + record.board.units
+            avatar.setRandomDrawable(record.user)
         }
     }
 }
+
+
---
+++ app/src/main/java/cz/leaderboard/app/presentation/common/AvatarUtils.kt
@@ -0,0 +1,29 @@
+package cz.leaderboard.app.presentation.common
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import cz.leaderboard.app.R
+import cz.leaderboard.app.data.model.User
+import de.hdodenhof.circleimageview.CircleImageView
+import java.util.*
+
+/**
+ * Created by semanticer on 18.06.2017.
+ */
+
+public fun CircleImageView.setRandomDrawable(user: User) {
+    this.setImageDrawable(getRandomDrawable(this.context, user.name.length))
+}
+
+fun getRandomDrawable(context: Context, randomSeed: Int): Drawable {
+    val l = listOf<Int>(
+            R.drawable.first,
+            R.drawable.third,
+            R.drawable.fourth,
+            R.drawable.fift,
+            R.drawable.sixyh,
+            R.drawable.seventh
+            )
+    return context.resources.getDrawable(l[randomSeed % 6])
+}
+
